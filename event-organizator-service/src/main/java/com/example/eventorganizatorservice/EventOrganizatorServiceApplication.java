49d37c5688e7df609ca3f5ed363b98f64669eb57
Loha21 <uroslohinski@gmail.com>
2024-04-22 14:05:59 +0200

    first

---
+++ event-organizator-service/src/main/java/com/example/eventorganizatorservice/EventOrganizatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventorganizatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrganizatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventOrganizatorServiceApplication.class, args);
+	}
+
+}
---
+++ event-organizator-service/src/test/java/com/example/eventorganizatorservice/EventOrganizatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventorganizatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventOrganizatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
