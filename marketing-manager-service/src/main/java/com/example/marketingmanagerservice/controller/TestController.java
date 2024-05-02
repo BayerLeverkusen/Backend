38e982861cfa70e7c671484a8dc0d21057bb9e32
Aleksa Simeunovic <84989791+aleksasimeunovic@users.noreply.github.com>
2024-05-02 09:21:05 +0200

    Initial service changes

--- marketing-manager-service/src/main/java/com/example/marketingmanagerservice/controller/TestController.java
+++ marketing-manager-service/src/main/java/com/example/marketingmanagerservice/controller/TestController.java
@@ -1,4 +1,18 @@
 package com.example.marketingmanagerservice.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/8052")
 public class TestController {
+
+    @PostMapping("/kurac")
+    public String register() {
+        String nesto = "kurac";
+        return nesto;
+    }
 }
