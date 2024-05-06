cadc10bfcbfcc62f6d9cd843b9a8893a2b0bf020
Loha21 <uroslohinski@gmail.com>
2024-05-06 20:09:19 +0200

    controller

--- event-organizator-service/src/main/java/com/example/eventorganizatorservice/repository/HotelRepository.java
+++ event-organizator-service/src/main/java/com/example/eventorganizatorservice/repository/HotelRepository.java
@@ -4,6 +4,7 @@ import com.example.eventorganizatorservice.model.Hotel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,7 +16,7 @@ public interface HotelRepository extends CrudRepository<Hotel, Integer> {
 
     Optional<Hotel> findByName(String name);
 
-    Iterable<Hotel> findAllByCity (String city);
+    List<Hotel> findAllByCity (String city);
 
     Iterable<Hotel> findAllByCountry (String country);
 
---
+++ event-organizator-service/src/main/java/com/example/eventorganizatorservice/service/HotelService.java
@@ -0,0 +1,35 @@
+package com.example.eventorganizatorservice.service;
+
+import com.example.eventorganizatorservice.dtos.HotelDto;
+import com.example.eventorganizatorservice.dtos.HotelRequest;
+import com.example.eventorganizatorservice.model.Hotel;
+import com.example.eventorganizatorservice.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class HotelService {
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    public List<HotelDto> getAllHotels(HotelRequest hotelRequest) {
+        List<Hotel> hotels = hotelRepository.findAllByCity(hotelRequest.getCity());
+        return hotels.stream().map(this::convertToDto).collect(Collectors.toList());
+
+    }
+
+    private HotelDto convertToDto(Hotel hotel) {
+        return HotelDto.builder()
+                .hotelName(hotel.getName())
+                .hotelCity(hotel.getCity())
+                .hotelCountry(hotel.getCountry())
+                .hotelPrice(hotel.getPrice())
+                .hotelRating(hotel.getRating())
+                .build();
+    }
+
+}
